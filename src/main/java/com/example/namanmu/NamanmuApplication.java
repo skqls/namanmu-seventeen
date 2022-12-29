de5f8b1a748fd86b890b8c0ba8e337b0d9169e0e
skqls <kitchin105@gmail.com>
2022-12-29 20:02:41 +0900

    Initial commit

---
+++ src/main/java/com/example/namanmu/NamanmuApplication.java
@@ -0,0 +1,13 @@
+package com.example.namanmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamanmuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamanmuApplication.class, args);
+	}
+
+}
