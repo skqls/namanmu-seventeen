85422fe1e56ce29ca8706b60eb5d87d93a4fe988
skqls <kitchin105@gmail.com>
2022-12-30 23:13:28 +0900

    12월30일

--- src/main/java/com/example/namanmu/Application.java
+++ src/main/java/com/example/namanmu/Application.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NamanmuApplication {
+public class Application {
 
+//	springapplication.run()가 was실행
 	public static void main(String[] args) {
-		SpringApplication.run(NamanmuApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
