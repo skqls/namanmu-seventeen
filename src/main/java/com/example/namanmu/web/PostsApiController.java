b88c0ba82cbdd2bc5e29d0acdc9e06ddb32d0e02
skqls <kitchin105@gmail.com>
2022-12-31 15:39:31 +0900

    fixed untracked files

---
+++ src/main/java/com/example/namanmu/web/PostsApiController.java
@@ -0,0 +1,39 @@
+//package com.example.namanmu.web;
+//
+//import com.example.namanmu.service.posts.PostsService;
+//import com.example.namanmu.web.dto.PostsSaveRequestDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RequiredArgsConstructor
+//@RestController
+//public class PostsApiController {
+//
+//    private final PostsService postsService;
+//
+//    @PostMapping("/api/v1/posts")
+//    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+//        return postsService.save(requestDto);
+//    }
+//
+////    @PutMapping("/api/v1/posts/{id}")
+////    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+////        return postsService.update(id, requestDto);
+////    }
+//
+//    @DeleteMapping("/api/v1/posts/{id}")
+//    public Long delete(@PathVariable Long id) {
+//        postsService.delete(id);
+//        return id;
+//    }
+////
+////    @GetMapping("/api/v1/posts/{id}")
+////    public PostsResponseDto findById(@PathVariable Long id) {
+////        return postsService.findById(id);
+////    }
+//
+////    @GetMapping("/api/v1/posts/list")
+////    public List<PostsListResponseDto> findAll() {
+////        return postsService.findAllDesc();
+////    }
+//}
